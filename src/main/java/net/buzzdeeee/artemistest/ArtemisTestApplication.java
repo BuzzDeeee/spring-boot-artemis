c58f8c1207d689265d428967757ab6c970253220
Seraph <seraph@orakel.de>
2023-03-20 15:34:00 +0100

    remove unused bean

--- src/main/java/net/buzzdeeee/artemistest/ArtemisTestApplication.java
+++ src/main/java/net/buzzdeeee/artemistest/ArtemisTestApplication.java
@@ -9,9 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
 
 @SpringBootApplication
 @EnableJms
@@ -36,14 +33,5 @@ public class ArtemisTestApplication {
         factory.setSubscriptionShared(true);
 
         return factory;
-    }
-
-
-    @Bean // Serialize message content to json using TextMessage
-    public MessageConverter jacksonJmsMessageConverter() {
-        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-        converter.setTargetType(MessageType.TEXT);
-        converter.setTypeIdPropertyName("_type");
-        return converter;
     }
 }
